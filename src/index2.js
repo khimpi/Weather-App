9d9d3b8eb5313711fbc06781aae2572e1bd9cf03
KD <65683045+khimpi@users.noreply.github.com>
2023-06-28 15:29:40 +0530

    Modified the JS file

--- src/index2.js
+++ src/index2.js
@@ -1,52 +1,52 @@
 // Display current date and time
 
-let now = new Date();
+function formatDateTime() {
 
-let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-day = day[now.getDay()];
+  let now = new Date();
 
-let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-month = month[now.getMonth()];
+  let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  day = day[now.getDay()];
 
+  let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  month = month[now.getMonth()];
 
-
-let cHour = now.getHours();
-let cMinute = now.getMinutes();
-
-if (cMinute > 9) { currentTime = `${cHour}:${cMinute}`; }
-else { currentTime = `${cHour}:0${cMinute}`; }
+  let cHour = now.getHours();
+  let cMinute = now.getMinutes();
+  let currentTime = `${cHour}:${cMinute}`;
 
 
-function dateOrdinalNumber() {
+  if (cMinute > 9) { currentTime = `${cHour}:${cMinute}`; }
+  else { currentTime = `${cHour}:0${cMinute}`; }
+
+
+  function dateOrdinalNumber() {
     if (date === 1 || date === 21 || date === 31) { return (date + "st"); }
     else if (date === 2 || date === 22) {
-        return (date + "nd");
+      return (date + "nd");
     }
     else if (date === 3) { return (date + "rd"); }
-    else if (3 < date < 21 ){ return (date + "th"); }
+    else if (3 < date < 21) { return (date + "th"); }
+  }
+
+  let date = now.getDate();
+  let currentDate = dateOrdinalNumber();
+
+  document.querySelector(".date").innerHTML = `${day} ${currentDate} ${month} | ${currentTime}`;
+  document.querySelector(".update").innerHTML = `${day} ${currentTime}`;
+
 }
 
-let date = now.getDate();
-currentDate = dateOrdinalNumber();
-
-let currentDateTime = document.querySelector(".date");
-currentDateTime.innerHTML = `${day} ${currentDate} ${month} | ${currentTime}`;
-
-
-// function showCityTemp(response) {
- 
-//   document.querySelector(".current-temp").innerHTML = Math.round(response.data.main.temp);
-
-// }
-
-
-
-
 function showCurrentCityTemp(response) {
+  cTemp = Math.round(response.data.main.temp);
   document.querySelector(".current-temp").innerHTML = Math.round(response.data.main.temp);
-  document.querySelector(".city").innerHTML = `${response.data.name}, `;
-   document.querySelector(".country").innerHTML = response.data.sys.country;
   
+  document.querySelector(".city").innerHTML = `${response.data.name}, `;
+  document.querySelector(".country").innerHTML = response.data.sys.country;
+  document.querySelector(".humidity").innerHTML = `${response.data.main.humidity}%`;
+  document.querySelector(".wind").innerHTML = `${response.data.wind.speed}km/h`;
+  document.querySelector(".max").innerHTML = `${Math.round(response.data.main.temp_max)}°C`;
+  document.querySelector(".min").innerHTML = `${Math.round(response.data.main.temp_min)}°C`;
+  formatDateTime(response.data.dt * 1000);
 }
 
 function showPosition(position) {
@@ -56,7 +56,6 @@ function showPosition(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
 
   axios.get(apiUrl).then(showCurrentCityTemp);
-  
 }
 
 function showCurrentLocation(event) {
@@ -71,16 +70,33 @@ function displayCity(event) {
   event.preventDefault();
   let cityName = document.querySelector(".form-control");
   document.querySelector(".city").innerHTML = cityName.value;
-  
 
   let unit = "metric";
   let apiKey = `b400ae3b711a616262d18b0ca2cbe78f`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=${apiKey}&units=${unit}`;
 
-
   axios.get(apiUrl).then(showCurrentCityTemp);
-  
 }
 
 let cityFormSubmit = document.querySelector("#city-form");
 cityFormSubmit.addEventListener("submit", displayCity);
+
+let cTemp = 36;
+
+function showTempC(event) {
+  event.preventDefault();
+  document.querySelector(".current-temp").innerHTML = cTemp;
+  
+}
+
+let degreeC = document.querySelector(".celcius-link");
+degreeC.addEventListener("click", showTempC);
+
+function showTempF(event) {
+  event.preventDefault();
+  document.querySelector(".current-temp").innerHTML = Math.round(cTemp * 9/7 + 32);
+}
+
+let degreeF = document.querySelector(".fahrenheit-link");
+degreeF.addEventListener("click", showTempF);
+
